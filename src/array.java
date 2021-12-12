0b292821d834ac414d2ed88983fb8ea5ca858a43
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-12 11:36:14 +0530

    commit

--- src/array.java
+++ src/array.java
@@ -6,28 +6,46 @@ public class array {
         int s1 = s.nextInt();
         values v = new values(s1);
         v.getvalues(s1);
+        v.sortarray();
     }
 }
 
 class values{
     private int n1;
-    private int[] a = new int[n1];
+    private int temp;
+    private int[] a;
         public values(int n)
         {
             n1 = n;
         }
         public void getvalues(int n1){
+            this.a = new int[n1];
             Scanner z = new Scanner(System.in);
             System.out.println("Enter the values : ");
             for(int i = 0; i < this.n1; i++)
             {
-                a[i] = z.nextInt();
+                this.a[i] = z.nextInt();
             }
             System.out.println("*******************");
             System.out.print("Entered values : ");
             for( int i = 0; i< this.n1; i++)
             {
 
+                System.out.print(a[i]+" ");
+            }
+        }
+        public void sortarray(){
+            for( int i =0; i<a.length; i++) {
+                for (int j = 0; j < a.length; j++) {
+                    if (a[i] < a[j]) {
+                        int temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+            System.out.print(" Sorted array : ");
+            for( int i = 0; i<n1; i++){
                 System.out.print(a[i]+" ");
             }
         }
