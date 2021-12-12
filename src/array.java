8ab47d023eae0fe04a221fdff66ae176f3ef45a4
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-12 11:08:52 +0530

    commit

---
+++ src/array.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class array {
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+        System.out.print("Enter no.of values to be sorted : ");
+        int s1 = s.nextInt();
+        values v = new values(s1);
+    }
+}
+
+class values{
+    private int n1;
+        public values(int n)
+        {
+            n1 = n;
+            getvalues(n1);
+        }
+        public void getvalues(int n1){
+            Scanner z = new Scanner(System.in);
+            System.out.println("Enter the values : ");
+            int[] a = new int[n1];
+            for(int i = 0; i < this.n1; i++)
+            {
+                a[i] = z.nextInt();
+            }
+            System.out.println("*******************");
+            System.out.print("Entered values : ");
+            for( int i = 0; i< this.n1; i++)
+            {
+
+                System.out.print(a[i]+" ");
+            }
+        }
+}
+
