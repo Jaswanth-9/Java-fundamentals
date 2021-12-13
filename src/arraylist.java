e2a406f61f9f28e59f6e21081480321d7e3f7c29
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-13 19:12:21 +0530

    commit

--- src/arraylist.java
+++ src/arraylist.java
@@ -2,19 +2,68 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class arraylist {
     public static void main(String[] args) {
-        mobile b = new mobile(" jaswanth", " 9874563210");
-        b.createcontact();
+        Scanner s = new Scanner(System.in);
+        System.out.println("1.Add contact"+"\n"+"9.Exit");
+        System.out.println("Choose your option : ");
+        int s3 = s.nextInt();
+        switch(s3){
+            case 1 :
+                System.out.print("Enter name : ");
+                String s1 = s.nextLine();
+                s.nextLine();
+                System.out.print("Enter number : ");
+                String s2 = s.nextLine();
+                contactvalues c = new contactvalues(s1,s2);
+                System.out.println(c.getname());
+                System.out.println(c.getnumber());
+            case 9 :
+                System.exit(-1);
+            default :
+                System.out.println("Enter valid option");
+                System.exit(-1);
+        }
     }
 }
 
-class mobile{
-    private String name;
-    private String phoneno;
-    public mobile( String n, String no){
-        name = n;
-        phoneno = no;
+class contactvalues{
+    private String na;
+    private String nu;
+    Scanner s = new Scanner(System.in);
+    public contactvalues( String name, String number){
+        na = name;
+        nu = number;
     }
-    public mobile createcontact(){
-        return new mobile(name, phoneno);
+
+    public String getname(){
+        return na;
+    }
+
+    public String getnumber(){
+        return nu;
+    }
+
+    public void getcontact(){
+        contactvalues v = new contactvalues(na,nu);
+    }
+
+}
+
+class lists{
+    private String number;
+    private ArrayList<contactvalues> contacts;
+    public lists(String number){
+        this.number = number;
+        this.contacts = new ArrayList<contactvalues>();
+    }
+
+    public void add(contactvalues contact){
+        if(position(contact) >= 0){
+            System.out.println("Entry already exists...!!!");
+        }
+        else this.contacts.add(contact);
+    }
+
+    public int position(contactvalues contact){
+        return this.contacts.indexOf(contact);
     }
 }
