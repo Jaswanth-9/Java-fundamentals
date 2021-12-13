fba6675f53c420839ac144babf0e655f966ba70c
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-13 11:48:44 +0530

    commit

---
+++ src/arraylist.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class arraylist {
+    public static void main(String[] args) {
+
+    }
+}
+
+class mobile{
+    public ArrayList<String> contact;
+    public void addcontact(String name, long number){
+        contact.add(name);
+    }
+}
